38462f953ef9980efaf193abcd53c03e43757c20
magnusjoshi2706 <mayurjoshi602@gmail.com>
2022-05-22 12:57:01 +0530

    this is my commit

---
+++ assign4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(){
+
+	int clientSocket, ret;
+	struct sockaddr_in serverAddr;
+	char buffer[1024];
+
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(8000);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+	while(1){
+		printf("Client: \t");
+		scanf("%s", &buffer[0]);
+		send(clientSocket, buffer, strlen(buffer), 0);
+
+		if(strcmp(buffer, ":exit") == 0){
+			close(clientSocket);
+			printf("Disconnected from server.\n");
+			exit(1);
+		}
+
+		//if(recv(clientSocket, buffer, 1024, 0) < 0){
+		//	printf("[-]Error in receiving data.\n");
+		//}
+                else
+                {
+			printf("Server: \t%s\n", buffer);
+		}
+	}
+
+	return 0;
+}
